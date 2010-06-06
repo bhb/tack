00d6736797248f4b862dba45ed2551f49507cfb6
Ben Brinckerhoff <ben@devver.net>
2010-06-06 08:58:07 -0600

    Fixed a bug in the BasicSummary formatter

--- lib/tack/formatters/basic_summary.rb
+++ lib/tack/formatters/basic_summary.rb
@@ -8,8 +8,7 @@ module Tack
       end
 
       def finish(results)
-        # 11 tests, 26 assertions, 0 failures, 0 errors
-        puts "%d tests, %d failures, %d pending" % [results.length, results[:failed].length, results[:pending].length]
+        puts "%d tests, %d failures, %d pending" % [results.values.flatten.length, results[:failed].length, results[:pending].length]
       end
       
     end
