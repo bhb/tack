210e023afea1152f4ae123d759df9ec6cbc51b9b
Ben Brinckerhoff <ben@devver.net>
2010-06-07 09:07:58 -0600

    Moved some 'require' calls to 'autoload' calls

--- lib/tack.rb
+++ lib/tack.rb
@@ -1,22 +1,17 @@
 libdir = File.dirname(__FILE__)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 
-require 'tack/test_set'
-require 'tack/runner'
-require 'tack/adapters/adapter'
-require 'tack/test_pattern'
-
 module Tack
 
-  #autoload :Runner, 'tack/runner'
-  #autoload :TestSet, 'tack/test_set'
+  autoload :Runner, 'tack/runner'
+  autoload :TestSet, 'tack/test_set'
   autoload :Middleware, 'tack/middleware'
-  #autoload :TestPattern, 'tack/test_pattern'
+  autoload :TestPattern, 'tack/test_pattern'
   
 
   module Adapters
 
-    #autoload :Adapter, 'tack/adapters/adapter'
+    autoload :Adapter, 'tack/adapters/adapter'
     autoload :RSpecAdapter, 'tack/adapters/rspec_adapter'
     autoload :TestUnitAdapter, 'tack/adapters/test_unit_adapter'
 
