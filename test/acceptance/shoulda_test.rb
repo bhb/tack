590c82aca8189b870c966854971945667bad2c0d
Ben Brinckerhoff <ben@devver.net>
2010-06-09 11:07:50 -0600

    Added a test for Shoulda contexts, which sort of work out of the box

--- test/acceptance/shoulda_test.rb
+++ test/acceptance/shoulda_test.rb
@@ -4,43 +4,54 @@ class ShouldaTest < Test::Unit::TestCase
   include TestHelpers
   
   should "grab all tests" do
-     body =<<-EOS
+    body =<<-EOS
        should "do something 2" do
        end
 
        should "do something 1" do
        end
      EOS
-     with_test_class(:body => body, :class_name => 'FooTest') do |file_name, path|
-       set = Tack::TestSet.new(path.parent)
-       tests = set.tests_for(path)
-       assert_equal 2, tests.length
-       assert_equal [file_name, "test: Foo should do something 1. "], tests.first
-       assert_equal [file_name, "test: Foo should do something 2. "], tests.last
-     end
+    with_test_class(:body => body, :class_name => 'FooTest') do |file_name, path|
+      set = Tack::TestSet.new(path.parent)
+      tests = set.tests_for(path)
+      assert_equal 2, tests.length
+      assert_equal [file_name, "test: Foo should do something 1. "], tests.first
+      assert_equal [file_name, "test: Foo should do something 2. "], tests.last
+    end
   end
   
   should "foo" do
   end
 
-   should "grab all tests matching pattern" do
-      body =<<-EOS
+  should "grab all tests matching pattern" do
+    body =<<-EOS
        should "do something" do
        end
      
        should "do something else" do
        end
      EOS
-     with_test_class(:body => body, :class_name => 'FooTest') do |file_name, path|
-       set = Tack::TestSet.new(path.parent)
-       tests = set.tests_for(path, /else/)
-       assert_equal 1, tests.length
-       assert_equal [file_name, "test: Foo should do something else. "], tests.first
-     end
-   end
+    with_test_class(:body => body, :class_name => 'FooTest') do |file_name, path|
+      set = Tack::TestSet.new(path.parent)
+      tests = set.tests_for(path, /else/)
+      assert_equal 1, tests.length
+      assert_equal [file_name, "test: Foo should do something else. "], tests.first
+    end
+  end
 
-#   should "grab all tests in contexts that match pattern" do
-#     ## pending
-#   end
+  should "grab all tests in contexts that match pattern" do
+    body =<<-EOS
+      context "in some context" do
+        should "do something" do
+        end
+      end
+    EOS
+    with_test_class(:body => body, :class_name => 'FooTest') do |file_name, path|
+      set = Tack::TestSet.new(path.parent)
+      tests = set.tests_for(path, /in some context/)
+      assert_equal 1, tests.length
+      assert_equal [file_name, "test: in some context Foo should do something. "], tests.first
+    end
+  end
 
 end
