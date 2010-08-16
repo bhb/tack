3b4a2cc4d5e3d93cd5b6f7ec9f4aa64dd0dd8e0d
Ben Brinckerhoff <ben@bbrinck.com>
2010-08-16 09:37:54 -0600

    Added test for Fork middleware

---
+++ test/unit/middleware/fork_test.rb
@@ -0,0 +1,46 @@
+require 'test_helper'
+
+class ForkTest < Test::Unit::TestCase
+  include MiddlewareTestHelper
+  include Tack::Middleware
+
+  should "return test result" do
+    fake_middleware = stub_everything
+    middleware = Fork.new(fake_middleware, :output => StringIO.new)
+    test = build_test
+    expected_result = results_for([test])
+    fake_middleware.stubs(:run_test).returns(expected_result)
+    assert_equal expected_result, middleware.run_test(*test)
+  end
+
+  should "isolate side effects from each test" do
+    body = <<-EOS
+    def test_one
+      @@var = 1
+    end
+
+    def test_two
+     assert_nil defined?(@@var)
+    end
+    EOS
+    with_test_class :class_name => 'FakeTest', :body => body do |_, path|
+      tests = [[path.to_s, ['FakeTest'], 'test_one'],
+               [path.to_s, 'FakeTest', 'test_two']]
+      # check that forked, test state is isolated
+      forked_app = Tack::Runner.new(path.parent) do |runner|
+        runner.use Tack::Middleware::Fork, :output => StringIO.new
+      end
+      results = Tack::ResultSet.new(forked_app.run(tests))
+      assert_equal 2, results.length
+      assert_equal 0, results.failed.length
+
+      # check that un-forked, test state is not isolated
+      # this must be done after the forking test above
+      app = Tack::Runner.new(path.parent).to_app
+      results = Tack::ResultSet.new(app.run(tests))
+      assert_equal 2, results.length
+      assert_equal 1, results.failed.length
+    end
+  end
+
+end
