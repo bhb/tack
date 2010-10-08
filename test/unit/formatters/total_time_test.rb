05ac42f5c89e85f6c2fd9c512a0f43f32a430af9
Ben Brinckerhoff <ben@bbrinck.com>
2010-10-07 19:45:35 -0600

    Added test for TotalTime formatter

--- test/unit/formatters/total_time_test.rb
+++ test/unit/formatters/total_time_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class TotalTimeTest < Test::Unit::TestCase
   include FormatterTestHelper
   include MiddlewareTestHelper
+  include Tack::Util
 
   def middleware_class
     TotalTime
@@ -10,5 +11,14 @@ class TotalTimeTest < Test::Unit::TestCase
 
   should_behave_like_middleware
   should_behave_like_formatter
+
+  should "print total time" do
+    adapter = StubAdapter.new
+    adapter.pass(Test.make)
+    output = StringIO.new
+    middleware = TotalTime.new(adapter, :output => output)
+    middleware.run_suite([Test.make])
+    assert_match /Finished in \d+.\d+ seconds/, output.string
+  end
 
 end
