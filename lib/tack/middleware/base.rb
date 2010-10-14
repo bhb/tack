b26f623e27bf0b45d95bbf9081ab92f87c924cd6
Ben Brinckerhoff <ben@bbrinck.com>
2010-10-13 22:58:32 -0600

    Added app_chain method for debugging

--- lib/tack/middleware/base.rb
+++ lib/tack/middleware/base.rb
@@ -3,6 +3,8 @@ module Tack
   module Middleware
 
     module Base
+
+      attr_reader :app
       
       def initialize(app, options = {})
         @app = app
@@ -10,6 +12,16 @@ module Tack
           @app.root = self
         end
         @output = options.fetch(:output){ STDOUT }
+      end
+
+      def app_chain(chain=[])
+        chain << self.class
+        if app.respond_to?(:app_chain)
+          app.app_chain(chain)
+        else
+          chain << app.class
+        end
+        chain
       end
 
       def run_suite(tests)
