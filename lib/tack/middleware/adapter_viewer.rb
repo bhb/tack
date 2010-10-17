07fa790e752785e48cd0a7f124fd2b37ea2ab1bb
Ben Brinckerhoff <ben@bbrinck.com>
2010-10-16 23:37:33 -0600

    Made AdapterViewer consolidate report if all files in a directory use the same adapter. Added some pending tests

--- lib/tack/middleware/adapter_viewer.rb
+++ lib/tack/middleware/adapter_viewer.rb
@@ -19,13 +19,42 @@ module Tack
         files.each do |file|
           files_to_adapters[file] = Adapter.for(file).class
         end
+        files_to_adapters = consolidate(files_to_adapters)
         files_to_adapters.each do |file, klass|
-          @output.puts "#{file} will use #{klass}"
+          if File.directory?(file)
+            @output.puts "All files in directory #{file} use #{klass}"
+          else
+            @output.puts "#{file} uses #{klass}"
+          end
         end
+        
         ResultSet.new.to_basics
       end
 
+      private 
+
+    def consolidate(mapping)
+      parent_to_files = {}
+      mapping.keys.each do |file|
+        parent_to_files[Pathname(file).parent] ||= []
+        parent_to_files[Pathname(file).parent] << file
+      end
+      parent_to_files.each do |path, files|
+        first_adapter = Adapter.for(files.first).class
+        if files.all? { |file| first_adapter == Adapter.for(file).class }
+          files.each do |file|
+            mapping.delete(file)
+          end
+          mapping[path] = first_adapter
+        end
+      end
+      mapping
     end
+
+    def print(mapping)
+    end
+
+  end
 
   end
 end
