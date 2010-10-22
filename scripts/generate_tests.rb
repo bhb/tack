d87df26c2d30e58a9e47c0a973538107a189bdc3
Ben Brinckerhoff <ben@bbrinck.com>
2010-10-22 17:51:38 -0600

    Started a script to generate massive tests files for perf testing

---
+++ scripts/generate_tests.rb
@@ -0,0 +1,28 @@
+type = ARGV.shift
+filename = ARGV.shift
+
+def rspec_test(num)
+  <<-EOF
+  specify 'spec number #{num}' do
+  end
+  EOF
+end
+
+case type.to_s
+when 'rspec'
+
+  contents =<<-EOF
+  require 'spec'
+  describe "something" do
+    
+    #{(0...1000).map{|i| rspec_test(i)}.join("\n\n")}
+    
+  end
+  EOF
+else
+  raise "Unknown test type '#{type}'"
+end
+
+File.open(filename, 'w') do |f|
+  f << contents
+end
