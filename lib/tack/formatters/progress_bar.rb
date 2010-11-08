b5f58f47ef47eee14d7287bdcaa822a15a14efa5
Ben Brinckerhoff <ben@bbrinck.com>
2010-11-07 20:35:34 -0700

    Added some commented out code for future work on IO syncing

--- lib/tack/formatters/progress_bar.rb
+++ lib/tack/formatters/progress_bar.rb
@@ -10,6 +10,9 @@ module Tack
       def initialize(app, options = {})
         super
         @verbose = options.fetch(:verbose) { false }
+        # TODO - for perf and for correctness, don't set 
+        # the sync variable blindly. Use start/restore (commented out below)
+        # to either set it for each test or for each suite
         @output.sync = TRUE
       end
 
@@ -34,6 +37,19 @@ module Tack
       end
 
       private
+
+      #def start_sync_output
+      #    @old_sync, output.sync = output.sync, true if output_supports_sync
+      #end
+
+      #def restore_sync_output
+      #  output.sync = @old_sync if output_supports_sync and !output.closed?
+      #end
+
+      #def output_supports_sync
+      #  output.respond_to?(:sync=)
+      #end
+
 
       def print_char_for_results(results, char)
         results.each do
